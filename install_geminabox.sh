ece8ea5caa85aba318615e59448cb6d9fa30ced8
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2015-10-30 12:18:26 +0100

    switch to Thin rack server
    (Puma was giving me grief here)

--- install_geminabox.sh
+++ install_geminabox.sh
@@ -2,4 +2,4 @@
 source $APP_HOME/.rvm/scripts/rvm
 rvm gemset create geminabox
 rvm use 2.2.3@geminabox
-gem install puma geminabox
+gem install thin geminabox
--- rackup.sh
+++ rackup.sh
@@ -2,4 +2,4 @@
 source $APP_HOME/.rvm/scripts/rvm
 sudo chmod -Rv 777 /data
 rvm use 2.2.3@geminabox
-rackup -s Puma --host 0.0.0.0 --port 9292
+rackup -s Thin --host 0.0.0.0 --port 9292
